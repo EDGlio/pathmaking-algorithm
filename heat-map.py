e2fa554e4fb494b9fbc20c9a99763110273e40c2
EDGlio <101647244+EDGlio@users.noreply.github.com>
2022-04-03 11:52:43 +0200

    Added comments
    Added comments and organised code

--- heat-map.py
+++ heat-map.py
@@ -1,19 +1,19 @@
 import numpy as np
-import showMaps
 import pygame
 from pygame.locals import *
 import time
 
 
-# 100 - not done (empty)
-# 200 - obstacle/barrier
+
 
 # Define variables
 empty = 500
 barrier = 1000
 endPos = (6, 6)
 
-# Sample map
+
+''' Sample Maps '''
+
 map = [
   [empty, empty, empty, empty, empty, empty, empty, empty],
   [empty, empty, barrier, barrier, empty, empty, empty, empty],
@@ -36,18 +36,20 @@ white_map = [
   [empty, empty, empty, empty, empty, empty, empty, empty],
 ]
 
+''' INIT VARIABLES '''
+
 HEIGHT, WIDTH = 400, 400
 RECT_HEIGHT, RECT_WIDTH = 50, 50
 FPS = 20
+GRID_DIMENSIONS = HEIGHT/RECT_HEIGHT
+
+''' COLOURS '''
 
 WHITE = (255, 255, 255)
 BARRIER = (200, 50, 50)
-PATH = (0, 0, 0) # (230, 190, 138)
+PATH = (0, 0, 0)
 
-# file = 'tmw_desert_spacing.png'
-# image = pygame.image.load(file)
-# rect = image.get_rect()
-# print(image)
+''' INITIALISATIONS '''
 
 pygame.init()
 screen = pygame.display.set_mode((HEIGHT, WIDTH))
@@ -103,6 +105,9 @@ def find_path(heat_map, start_pos):
     render_tile(current_pos, PATH)
     time.sleep(0.5)
     try:
+      # First checks that you cannot go outside the map
+      # Then checks that you are going closer to the endpoint
+      # Changes position to that coordinate, and get the value at that place
       if current_pos[0] != 7 and map[current_pos[0] + 1][current_pos[1]] == current_val - 1:
         current_pos = [current_pos[0] + 1, current_pos[1]]
         current_val = map[current_pos[0]][current_pos[1]]
@@ -156,7 +161,6 @@ def heat_map(map, endPos):
       run = False
   return map
 
-# render_map(find_path(heat_map(map, endPos), [0, 0]))
 map = render_map(heat_map(map, endPos))
 
 time.sleep(2)
@@ -165,6 +169,7 @@ find_path(map, (1, 1))
 
 ''' GAME LOOP '''
 
+# Enables customisation
 customize = False
 
 while True:
@@ -172,20 +177,25 @@ while True:
     if event.type == pygame.QUIT:
       pygame.quit()
     if event.type == pygame.KEYDOWN:
-      if event.key == pygame.K_SPACE:
+      if event.key == pygame.K_SPACE: 
+        # Enables customisation
+        # Turns map fully white (empty)
         customize = True
         render_all_white()
         map = white_map.copy()
-      if event.key == pygame.K_RETURN:
+      if event.key == pygame.K_RETURN: 
+        # Disables customisation
+        # renders the heat map of the customised map
         customize = False
         map = render_map(heat_map(map, endPos))
       if event.key == pygame.K_p and customize == False:
+        # pathfinds the current heat map
         find_path(map, (1, 1))
     if event.type == pygame.MOUSEBUTTONUP:    
       if customize == True:
-        pos = [i//50 for i in pygame.mouse.get_pos()]
+        pos = [i//RECT_HEIGHT for i in pygame.mouse.get_pos()]
         map[pos[1]][pos[0]] = barrier
-        pos.reverse()
+        pos.reverse() # Pygame goes up to down, left to right
         render_tile(pos, BARRIER)
         
   FPSCLOCK.tick(FPS)
